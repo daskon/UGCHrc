1afd3756738f7faa55f2d3874a5a9fb5f5226177
Asela Daskon <aselaforever@gmail.com>
2017-08-09 15:55:10 +0530

    reoved line from violation controller

--- app/Http/Controllers/ComplainsController.php
+++ app/Http/Controllers/ComplainsController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Violations;
 
 class ComplainsController extends Controller
 {
     public function create()
     {
-       return view('complains.create');
+       $violation = Violations::all();
+
+       return view('complains.create',compact('violation'));
     }
 }
--- app/Http/Controllers/ViolationsController.php
+++ app/Http/Controllers/ViolationsController.php
@@ -3,14 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Violations;
 
 class ViolationsController extends Controller
 {
     public function show()
     {
-        $violation = Violations::all();
-
-        return view('complains.create',compact('violation'));
+        return view('complains.create');
     }
 }
